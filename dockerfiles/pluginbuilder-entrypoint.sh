1a9ac41d3b417f7418fee94d03a2ca3b3095c66e
jamesTait-jt <jamesatait12@gmail.com>
2024-10-24 13:24:03 +0100

    Remove logs in pluginbuilder entrypoint

--- dockerfiles/pluginbuilder-entrypoint.sh
+++ dockerfiles/pluginbuilder-entrypoint.sh
@@ -7,12 +7,6 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-ls -la
-cd handlers
-echo "in handlers!!!"
-ls -la
-pwd
-
 # Compile all Go files in the specified directory
 for file in "$1"/*.go; do
     # Get the base name without the extension for output
