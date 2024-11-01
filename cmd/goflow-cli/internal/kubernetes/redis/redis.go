36e6921552ff0f369069f65ba0008bfd4074c77e
James Tait <jamesatait12@gmail.com>
2024-11-01 15:54:36 +0000

    Reformat

--- cmd/goflow-cli/internal/kubernetes/redis/redis.go
+++ cmd/goflow-cli/internal/kubernetes/redis/redis.go
@@ -23,37 +23,41 @@ var (
 func Deployment(conf *config.Config) *acappsv1.DeploymentApplyConfiguration {
 	return acappsv1.Deployment(deploymentName, conf.Kubernetes.Namespace).
 		WithLabels(labels).
-		WithSpec(acappsv1.DeploymentSpec().
-			WithReplicas(conf.Redis.Replicas).
-			WithSelector(acmetav1.LabelSelector().WithMatchLabels(labels)).
-			WithTemplate(acapiv1.PodTemplateSpec().
-				WithLabels(labels).
-				WithSpec(acapiv1.PodSpec().
-					WithContainers(
-						acapiv1.Container().
-							WithName(deploymentContainerName).
-							WithImage(conf.Redis.Image).
-							WithPorts(
-								acapiv1.ContainerPort().
-									WithProtocol(apiv1.ProtocolTCP).
-									WithContainerPort(RedisPort),
-							),
-					),
+		WithSpec(
+			acappsv1.DeploymentSpec().
+				WithReplicas(conf.Redis.Replicas).
+				WithSelector(acmetav1.LabelSelector().WithMatchLabels(labels)).
+				WithTemplate(
+					acapiv1.PodTemplateSpec().
+						WithLabels(labels).
+						WithSpec(
+							acapiv1.PodSpec().
+								WithContainers(
+									acapiv1.Container().
+										WithName(deploymentContainerName).
+										WithImage(conf.Redis.Image).
+										WithPorts(
+											acapiv1.ContainerPort().
+												WithProtocol(apiv1.ProtocolTCP).
+												WithContainerPort(RedisPort),
+										),
+								),
+						),
 				),
-			),
 		)
 }
 
 func Service(conf *config.Config) *acapiv1.ServiceApplyConfiguration {
 	return acapiv1.Service(ServiceName, conf.Kubernetes.Namespace).
 		WithLabels(labels).
-		WithSpec(acapiv1.ServiceSpec().
-			WithSelector(labels).
-			WithType(apiv1.ServiceTypeClusterIP).
-			WithPorts(
-				acapiv1.ServicePort().
-					WithPort(RedisPort).
-					WithTargetPort(intstr.FromInt32(RedisPort)),
-			),
+		WithSpec(
+			acapiv1.ServiceSpec().
+				WithSelector(labels).
+				WithType(apiv1.ServiceTypeClusterIP).
+				WithPorts(
+					acapiv1.ServicePort().
+						WithPort(RedisPort).
+						WithTargetPort(intstr.FromInt32(RedisPort)),
+				),
 		)
 }
