4cf0832849069b14c9b899379c1bbf5a10be2644
James Tait <jamesatait12@gmail.com>
2024-11-14 15:00:43 +0000

    Add doc comments for redis_broker

--- broker/redis_broker.go
+++ broker/redis_broker.go
@@ -15,11 +15,17 @@ type redisClient interface {
 	BRPop(ctx context.Context, timeout time.Duration, keys ...string) *redis.StringSliceCmd
 }
 
+// Encoder defines methods for serializing and deserializing tasks of type T, where
+// T satisfies task.TaskOrResult. This allows tasks and results to be encoded for
+// sending over Redis and decoded when retrieved.
 type Encoder[T task.TaskOrResult] interface {
 	Serialise(toSerialise T) ([]byte, error)
 	Deserialise(toDeserialise []byte) (T, error)
 }
 
+// RedisBroker is a Redis-backed message broker that supports submitting and
+// asynchronously retrieving tasks of type T. It provides a channel-based interface for
+// consuming tasks and includes options for configuring logging.
 type RedisBroker[T task.TaskOrResult] struct {
 	client        redisClient
 	redisQueueKey string
@@ -30,6 +36,8 @@ type RedisBroker[T task.TaskOrResult] struct {
 	opts          redisBrokerOptions
 }
 
+// NewRedisBroker creates a new RedisBroker instance with the specified Redis client, queue key,
+// and Encoder.
 func NewRedisBroker[T task.TaskOrResult](
 	client redisClient,
 	key string,
@@ -52,6 +60,8 @@ func NewRedisBroker[T task.TaskOrResult](
 	}
 }
 
+// Submit serializes a task and pushes it to the Redis queue. If serialization or
+// pushing fails, Submit returns an error.
 func (rb *RedisBroker[T]) Submit(ctx context.Context, submission T) error {
 	serialised, err := rb.encoder.Serialise(submission)
 	if err != nil {
@@ -66,6 +76,11 @@ func (rb *RedisBroker[T]) Submit(ctx context.Context, submission T) error {
 	return nil
 }
 
+// Dequeue returns a receive-only channel that emits tasks as they are retrieved from
+// the Redis queue. Dequeue starts a background goroutine for polling tasks from the
+// queue. The polling process stops when the provided context is canceled. errors are
+// logged but they will not stop the polling loop. pollRedis exits when the provided
+// context is cancelled.
 func (rb *RedisBroker[T]) Dequeue(ctx context.Context) <-chan T {
 	rb.started.Do(func() {
 		rb.wg.Add(1)
@@ -101,6 +116,8 @@ func (rb *RedisBroker[T]) pollRedis(ctx context.Context) {
 	}
 }
 
+// AwaitShutdown waits for the background polling goroutine to finish.
+// This method should be called during shutdown to ensure all resources are released.
 func (rb *RedisBroker[T]) AwaitShutdown() {
 	rb.wg.Wait()
 }
